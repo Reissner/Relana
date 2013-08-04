e1bc437f46afae4523e8719e1253e115faff01a7
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2013-08-04 13:49:16 +0000

    @bugfix 	metho toString() now allows null-form.
    git-svn-id: file:///SVN_Archive/Software/trunk@6713 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/eu/simuline/relana/model/SInstance.java
+++ src/main/java/eu/simuline/relana/model/SInstance.java
@@ -146,7 +146,7 @@ public class SInstance {
 	res.append("\n<SInstance name=\"");
 	res.append(this.name);
 	res.append("\">\nformula: ");
-	res.append(this.form.toString());
+	res.append(""+this.form);
 	res.append("\n</SInstance>");
 
 	return res.toString();
