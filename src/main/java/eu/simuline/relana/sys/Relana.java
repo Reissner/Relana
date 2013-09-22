904bc654d12975656b78d9373abf3dfdd6ba8f8d
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2013-09-22 21:29:34 +0000

    @chg 	minor.
    git-svn-id: file:///SVN_Archive/Software/trunk@6981 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/eu/simuline/relana/sys/Relana.java
+++ src/main/java/eu/simuline/relana/sys/Relana.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 
 //import java.util.Iterator;
 import java.util.Set;
-//import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
