f8d40d269dac2e769615a79e2705ea2615b20712
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2013-10-01 20:39:32 +0000

    @doc 	removed old comment "eq&hashCode not overwritten"
    git-svn-id: file:///SVN_Archive/Software/trunk@7027 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/eu/simuline/relana/model/FlatCInstance.java
+++ src/main/java/eu/simuline/relana/model/FlatCInstance.java
@@ -128,7 +128,7 @@ public class FlatCInstance {
 	// create the set {def} with type serv.getType() 
 	// This works, because def is minimal within serv.getType() 
 	Set<Deficiency> defSet = 
-	    new HashSet<Deficiency>();// eq&hashCode not overwritten 
+	    new HashSet<Deficiency>();
 	defSet.add(def);
 	Formula newConst = new Formula.Const(defSet,serv.getType());
 	assert serv.getType().isValid(defSet);
