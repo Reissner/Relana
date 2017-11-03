c79624bf8b044dde89d43ab99b116e01cb4fe0ad
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2017-11-03 23:47:15 +0000

    @new 	site: javadoc 	- added package description.
    git-svn-id: file:///SVN_Archive/Software/trunk@12405 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/main/java/eu/simuline/relana/parser/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Classes to parse all file types except xml: 
+ * {@link CClassParser} parse <code>*.ccl</code> files 
+ * representing Component Classes, 
+ * {@link SClassParser} parse <code>*.scl</code> files 
+ * representing Service Classes and finally 
+ * {@link FormulaParser} parse formulae. 
+ *
+ * <code>x</code> ***** NOT YET COMPLETE. 
+ */
+package eu.simuline.relana.parser;
+
