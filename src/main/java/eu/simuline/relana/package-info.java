2ec02647172bf9a05b8d7d4a3a177714fa332ecc
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2017-11-08 03:21:05 +0000

    @new 	not rendered.
    git-svn-id: file:///SVN_Archive/Software/trunk@12429 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/main/java/eu/simuline/relana/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Comprises all classes for Relana 
+ * without general helper classes. 
+ *
+ * <code>x</code> ***** NOT YET COMPLETE. 
+ * DOES NOT OCCUR in the created javadocs 
+ */
+package eu.simuline.relana;
+
