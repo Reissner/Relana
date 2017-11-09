bee77ad27239ce883079214acf20472c81625e70
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2017-11-09 02:47:50 +0000

    @new
    git-svn-id: file:///SVN_Archive/Software/trunk@12452 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/main/java/eu/simuline/relana/sys/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Relana as a system comprising the main class {@link Relana} 
+ * and the classes {@link Project} and {@link ProjectDesc} 
+ * to read and to represent a project file. 
+ *
+ * <code>x</code> ***** NOT YET COMPLETE. 
+ */
+package eu.simuline.relana.sys;
