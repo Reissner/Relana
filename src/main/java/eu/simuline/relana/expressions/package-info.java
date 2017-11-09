5b2408ff31532cdc817e20776f8495035898685b
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2017-11-09 02:36:11 +0000

    @doc
    git-svn-id: file:///SVN_Archive/Software/trunk@12450 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/main/java/eu/simuline/relana/expressions/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Classes to describe expressions. 
+ * Expressions come as {@link Formula}e 
+ * and as their declarations {@link FormulaDecl}s. 
+ * Formulae are either atomic, i.e. constants or variables 
+ * or they are defined in terms of {@link Operation}s. 
+ * Formulae are typed: variables, constants and operations 
+ * both their parameter and their return value. 
+ * Types are represented by {@link Type}s. 
+ *
+ * <code>x</code> ***** NOT YET COMPLETE. 
+ */
+package eu.simuline.relana.expressions;
